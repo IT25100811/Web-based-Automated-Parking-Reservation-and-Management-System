80d7adfda8aa9c59010977e4d91d772cf6fbe364
Akindu Menuja <it25100811@my.sliit.lk>
2026-08-15 16:59:17 +0530

    Initial commit

---
+++ src/main/java/Web_Based_Automated_Parking_Reservation_and_Management_System_backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package Web_Based_Automated_Parking_Reservation_and_Management_System_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Web_Based_Automated_Parking_Reservation_and_Management_System_backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package Web_Based_Automated_Parking_Reservation_and_Management_System_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
